fab1146814b725bfdcd822efe467c0748e67bc7f
victor <tsekwanwai2009@gmail.com>
2024-05-12 19:05:05 +0800

    update take photo

--- components/Home.js
+++ components/Home.js
@@ -1,23 +1,107 @@
-import {View, StyleSheet } from 'react-native';
-import { useNavigation } from '@react-navigation/native'
-import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
-import defaultStyles from './../components/styles/DefaultStyles';
-
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Camera } from "expo-camera";
+import defaultStyles from "./../components/styles/DefaultStyles";
+import { useState } from "react";
+import * as ImagePicker from "expo-image-picker";
+import { FirebaseMLVision } from "@react-native-firebase/ml-vision";
 
 export default function Home() {
+  const [image, setImage] = useState(null);
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
 
-  const navigation = useNavigation()
-    
-  return (
-      <View style={defaultStyles.container}>
-       
+    // if (result.assets[0]) {
+    setImage(result.assets[0].uri);
+    // }
+
+    try {
+      const processedImage =
+        await FirebaseMLVision.cloudDocumentTextRecognizerProcessImage(image);
+      const recognizedText = processedImage.text;
+      console.log(recognizedText);
+    } catch (error) {
+      console.error("Error processing image for OCR:", error);
+    }
+  };
+  const [facing, setFacing] = useState("back");
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: "center" }}>
+          We need your permission to show the camera
+        </Text>
+        <Button onPress={requestPermission} title="grant permission" />
       </View>
-  )
+    );
+  }
+  function toggleCameraFacing() {
+    setFacing((current) => (current === "back" ? "front" : "back"));
+  }
+  const navigation = useNavigation();
+
+  return (
+    <View style={defaultStyles.container}>
+      {/* <Camera style={styles.camera} facing={facing}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera> */}
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && <Image source={{ uri: image }} style={styles.image} />}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-
-})
-
-
-  
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "transparent",
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+});
