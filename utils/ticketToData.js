5e4d1f24d4895b1e5d7b6645e6e7aaabcd277b7d
victor <tsekwanwai2009@gmail.com>
2024-05-16 00:08:40 +0800

    ticketToData (tbc)

--- utils/ticketToData.js
+++ utils/ticketToData.js
@@ -1,8 +1,39 @@
 export default function ticketToData(ocrText) {
+  var date = "";
+  var time = "";
+  var brand = "";
+  const stringWithoutSpaces = ocrText.replace(/\s/g, "");
+
+  const dateRegex = /\d{2}\/\d{2}\/\d{4}/;
+  const dateMatch = stringWithoutSpaces.match(dateRegex);
+  if (dateMatch) {
+    const date = dateMatch[0];
+    console.log(date);
+  } else {
+    console.log("No date found.");
+  }
+
+  const timeRegex = /\d{2}:\d{2}(am|pm)/gi;
+  const timeMatch = stringWithoutSpaces.match(timeRegex);
+  if (timeMatch) {
+    const time = timeMatch[0];
+    console.log(time);
+  } else {
+    console.log("No time found.");
+  }
+
+  if (stringWithoutSpaces.includes("MCL")) {
+    brand = "mcl";
+  } else if (stringWithoutSpaces.includes("broadway")) {
+    brand = "broadway";
+  } else {
+    console.log("No brand found.");
+  }
+
   return {
     film: "",
-    date: "",
-    time: "",
+    date: date,
+    time: time,
     cinema: "",
     house: "",
     seat: "",
