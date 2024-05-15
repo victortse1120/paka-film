4ef96d52217e525119f7b0b258561f1f54eb05ed
victor <tsekwanwai2009@gmail.com>
2024-05-15 11:21:06 +0800

    ocr

--- components/Home.js
+++ components/Home.js
@@ -9,12 +9,67 @@ import {
 import { useNavigation } from "@react-navigation/native";
 import { Camera } from "expo-camera";
 import defaultStyles from "./../components/styles/DefaultStyles";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import * as ImagePicker from "expo-image-picker";
-import { FirebaseMLVision } from "@react-native-firebase/ml-vision";
 
 export default function Home() {
   const [image, setImage] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    ocr();
+  }, [image]);
+
+  const ocr = async () => {
+    if (image) {
+      try {
+        const response = await fetch(
+          "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyCBeBFhTSIMdYA0jLwwvzRak5e1gzqN4EQ",
+          {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              requests: [
+                {
+                  image: {
+                    content: image,
+                  },
+                  features: [
+                    {
+                      type: "TEXT_DETECTION",
+                    },
+                  ],
+                },
+              ],
+            }),
+          }
+        );
+        const data = await response.json();
+        if (data.responses && data.responses.length > 0) {
+          const result = data.responses[0];
+
+          if (result.error) {
+            console.log(result.error.message);
+          }
+          if (result.fullTextAnnotation && result.fullTextAnnotation.text) {
+            const recognizedText = result.fullTextAnnotation.text;
+            console.log("Recognized Text:", recognizedText);
+          } else {
+            console.log("No text found in the image");
+          }
+        } else {
+          console.log("Invalid API response");
+        }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+  };
+
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -22,24 +77,28 @@ export default function Home() {
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
+      base64: true,
     });
 
-    // if (result.assets[0]) {
-    setImage(result.assets[0].uri);
-    // }
-
-    try {
-      const processedImage =
-        await FirebaseMLVision.cloudDocumentTextRecognizerProcessImage(image);
-      const recognizedText = processedImage.text;
-      console.log(recognizedText);
-    } catch (error) {
-      console.error("Error processing image for OCR:", error);
-    }
+    setImage(result.assets[0].base64);
   };
+
   const [facing, setFacing] = useState("back");
   const [permission, requestPermission] = Camera.useCameraPermissions();
 
+  return (
+    <View style={defaultStyles.container}>
+      {/* <Camera style={styles.camera} facing={facing}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera> */}
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && <Image source={{ uri: image }} style={styles.image} />}
+    </View>
+  );
   if (!permission) {
     // Camera permissions are still loading.
     return <View />;
@@ -60,20 +119,6 @@ export default function Home() {
     setFacing((current) => (current === "back" ? "front" : "back"));
   }
   const navigation = useNavigation();
-
-  return (
-    <View style={defaultStyles.container}>
-      {/* <Camera style={styles.camera} facing={facing}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-            <Text style={styles.text}>Flip Camera</Text>
-          </TouchableOpacity>
-        </View>
-      </Camera> */}
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && <Image source={{ uri: image }} style={styles.image} />}
-    </View>
-  );
 }
 
 const styles = StyleSheet.create({
