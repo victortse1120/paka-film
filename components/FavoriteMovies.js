85a1d88899e4310a942a1723f35678514fcc61b3
victor <tsekwanwai2009@gmail.com>
2024-05-25 22:43:25 +0800

    update Order.js tabs

--- components/FavoriteMovies.js
+++ components/FavoriteMovies.js
@@ -1,13 +1,5 @@
 import React from "react";
-import {
-  View,
-  Text,
-  Image,
-  FlatList,
-  TouchableOpacity,
-  StyleSheet,
-} from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import { View, Text, Image, FlatList, StyleSheet } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 
 const movies = [
@@ -76,125 +68,9 @@ const FavoriteMovies = () => (
   />
 );
 
-// const MovieItem = ({ item }) => {
-//   const navigation = useNavigation();
-
-//   return (
-//     <TouchableOpacity
-//       onPress={() => navigation.navigate("ReviewDetail", { review: item })}
-//     >
-//       <View style={styles.reviewContainer}>
-//         <View style={styles.titleContainer}>
-//           <Text style={styles.reviewTitle}>{item.title}</Text>
-//           <FontAwesome
-//             name="heart"
-//             size={20}
-//             color="#FFC800"
-//             style={styles.icon}
-//           />
-//         </View>
-
-//         <View style={styles.detailContainer}>
-//           <Text style={styles.reviewSubTitle}>{item.subTitle}</Text>
-//           <Text style={styles.reviewContent}>{item.content}</Text>
-//         </View>
-
-//         <View style={styles.authorContainer}>
-//           <Image source={authorIcon} style={styles.authorIcon} />
-//           <Text style={styles.reviewAuthor}>
-//             {item.author} {item.date}
-//           </Text>
-//         </View>
-//       </View>
-//     </TouchableOpacity>
-//   );
-// };
-
-// const ReviewList = () => (
-//   <FlatList
-//     data={reviews}
-//     renderItem={({ item, index }) => (
-//       <View>
-//         {index > 0 && (
-//           <View
-//             style={{
-//               borderTopWidth: 1,
-//               borderTopColor: "#969696",
-//               marginVertical: 20,
-//             }}
-//           />
-//         )}
-//         <ReviewItem item={item} />
-//       </View>
-//     )}
-//     keyExtractor={(item) => item.id}
-//     contentContainerStyle={styles.listContainer}
-//   />
-// );
-
-// const styles = StyleSheet.create({
-//   listContainer: {
-//     padding: 20,
-//   },
-//   movieContainer: {
-//     padding: 20,
-//     paddingHorizontal: 16,
-//     marginVertical: 0,
-//     backgroundColor: "#2D2D2D",
-//     borderRadius: 12,
-//   },
-//   titleContainer: {
-//     flexDirection: "row",
-//     justifyContent: "space-between",
-//     alignItems: "center",
-//   },
-//   reviewTitle: {
-//     fontSize: 18,
-//     fontWeight: "bold",
-//     color: "#fff",
-//   },
-//   icon: {
-//     marginLeft: 10,
-//   },
-
-//   detailContainer: {
-//     backgroundColor: "#3C3C3C",
-//     padding: 12,
-//     borderRadius: 8,
-//     marginVertical: 16,
-//   },
-//   reviewSubTitle: {
-//     fontSize: 16,
-//     color: "white",
-//     fontWeight: "bold",
-//     lineHeight: 24,
-//     marginVertical: 10,
-//   },
-//   reviewContent: {
-//     fontSize: 14,
-//     color: "#969696",
-//     marginVertical: 10,
-//     lineHeight: 21,
-//   },
-//   authorContainer: {
-//     flexDirection: "row",
-//     alignItems: "center",
-//     justifyContent: "flex-end",
-//   },
-//   authorIcon: {
-//     width: 20,
-//     height: 20,
-//     marginRight: 8,
-//   },
-//   reviewAuthor: {
-//     fontSize: 13,
-//     color: "#fff",
-//   },
-// });
-
 const styles = StyleSheet.create({
   listContainer: {
-    padding: 20,
+    paddingVertical: 24,
   },
   movieContainer: {
     flexDirection: "row",
--- pages/Order.js
+++ pages/Order.js
@@ -1,56 +1,26 @@
 import { useState } from "react";
-import { View, Text, StyleSheet, Dimensions } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import FavoriteMovies from "../components/FavoriteMovies";
 import FavoriteReviews from "../components/FavoriteReviews";
-
-const TopTab = createMaterialTopTabNavigator();
+import MyTabs from "../components/Tab";
 
 export default function ReviewTabs() {
   const [active, setActive] = useState(0);
-  const [reviewNumbers, setReviewNumbers] = useState([3, 3]);
+  const [fovouriteNumbers, setFovouriteNumbers] = useState([3, 3]);
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Favorite</Text>
-      <TopTab.Navigator
-        screenOptions={{
-          tabBarIndicatorStyle: {
-            backgroundColor: "#FFC800",
-            width: 172,
-            height: 56,
-            borderRadius: 10,
-          },
-          tabBarStyle: {
-            backgroundColor: "#2D2D2D",
-            borderRadius: 12,
-            margin: 20,
-            marginBottom: 10,
-          },
-          tabBarLabelStyle: {
-            color: "grey",
-            textTransform: "none",
-            fontSize: 16,
-            fontWeight: "bold",
-          },
-          tabBarItemStyle: {
-            "&.Mui-selected": {
-              color: "black",
-            },
-          },
+      <MyTabs
+        titles={["Movies", "Reviews"]}
+        active={active}
+        onPress={(index) => {
+          setActive(index);
         }}
-      >
-        <TopTab.Screen
-          name="Movies"
-          component={FavoriteMovies}
-          options={{ tabBarLabel: "Movies (3)" }}
-        />
-        <TopTab.Screen
-          name="Reviews"
-          component={FavoriteReviews}
-          options={{ tabBarLabel: "Reviews (2)" }}
-        />
-      </TopTab.Navigator>
+        number={fovouriteNumbers}
+      />
+      {active == 0 ? <FavoriteMovies /> : <FavoriteReviews />}
     </View>
   );
 }
@@ -59,14 +29,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "black",
+    paddingHorizontal: 20,
   },
   title: {
     fontSize: 24,
     fontWeight: "bold",
     color: "white",
     textAlign: "left",
-    paddingLeft: 20,
-    marginTop: 40,
+    marginVertical: 16,
   },
   listContainer: {
     padding: 20,
